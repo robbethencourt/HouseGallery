063244eeeb50256e6137a0513842488619efb912
Robert Bethencourt <robert.bethencourt@gmail.com>
2016-11-20 16:19:43 -0500

    nav component built

--- src/elm/Main.elm
+++ src/elm/Main.elm
@@ -2,29 +2,17 @@ module Main exposing (..)
 
 import Html exposing (..)
 import Html.Attributes exposing (..)
-import Html.Events exposing (..)
-import Navigation
-import String
+import Nav
 
 
--- main
-
-
-main : Program Never Model Msg
-main =
-    Html.beginnerProgram
-        { model = initialModel
-        , view = view
-        , update = update
-        }
-
-
-
+-- import Navigation
+-- import String
 -- model
 
 
-type alias Model =
-    { user : UserModel
+type alias AppModel =
+    { navModel : Nav.Model
+    , user : UserModel
     , usersGallery : Gallery
     , galleryFeed : List GalleryItem
     }
@@ -60,39 +48,79 @@ type alias GalleryItem =
     }
 
 
-initialModel : Model
+initialModel : AppModel
 initialModel =
-    { user = { isAuthed = False, error = "", userId = { username = "", userEmail = "", password = "" } }
+    { navModel = Nav.initialModel
+    , user = { isAuthed = False, error = "", userId = { username = "", userEmail = "", password = "" } }
     , usersGallery = { error = "", galleries = [] }
     , galleryFeed = []
     }
 
 
+init : ( AppModel, Cmd Msg )
+init =
+    ( initialModel, Cmd.none )
+
+
 
 -- update
+-- type RemoteData e a
+--     = NotAsked
+--     | Loading
+--     | Success e
+--     | Failure a
 
 
 type Msg
-    = Input String
-    | Something
+    = NavMsg Nav.Msg
 
 
-update : Msg -> Model -> Model
+update : Msg -> AppModel -> ( AppModel, Cmd Msg )
 update msg model =
     case msg of
-        _ ->
-            model
+        NavMsg subMsg ->
+            let
+                ( updatedNavModel, navCmd ) =
+                    Nav.update subMsg model.navModel
+            in
+                ( { model | navModel = updatedNavModel }, Cmd.map NavMsg navCmd )
 
 
 
 -- view
 
 
-view : Model -> Html Msg
+view : AppModel -> Html Msg
 view model =
     div []
-        [ input [ type_ "text" ] []
-        , input [ type_ "text" ] []
-        , input [ type_ "button", value "Login" ] []
-        , p [] [ text (toString model) ]
+        [ Html.map NavMsg (Nav.view model.navModel)
+        , div []
+            [ input [ type_ "text" ] []
+            , input [ type_ "text" ] []
+            , p [] [ text (toString model) ]
+            ]
         ]
+
+
+
+-- subscriptions
+
+
+subscriptions : AppModel -> Sub Msg
+subscriptions model =
+    Sub.none
+
+
+
+-- app
+-- main
+
+
+main : Program Never AppModel Msg
+main =
+    program
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
---
+++ src/elm/Nav.elm
@@ -0,0 +1,50 @@
+module Nav exposing (..)
+
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (onClick)
+
+
+-- model
+
+
+type alias Model =
+    { isAuthed : Bool }
+
+
+initialModel : Model
+initialModel =
+    { isAuthed = False }
+
+
+
+-- update
+
+
+type Msg
+    = Login
+    | Logout
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        Login ->
+            ( { model | isAuthed = True }, Cmd.none )
+
+        Logout ->
+            ( { model | isAuthed = False }, Cmd.none )
+
+
+
+-- view
+
+
+view : Model -> Html Msg
+view model =
+    if model.isAuthed then
+        div []
+            [ input [ type_ "button", value "Logout", onClick Logout ] [] ]
+    else
+        div []
+            [ input [ type_ "button", value "Login", onClick Login ] [] ]
