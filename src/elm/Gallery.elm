a8f5f5a878d9b0d770396eb01a14f0bfecef3b73
Robert Bethencourt <robert.bethencourt@gmail.com>
2017-04-28 10:28:40 -0400

    can now switch between list and table view on gallery page

--- src/elm/Gallery.elm
+++ src/elm/Gallery.elm
@@ -18,6 +18,8 @@ type alias Model =
     , active : Bool
     , routeParam : String
     , isFetching : Bool
+    , listView : Bool
+    , tableView : Bool
     }
 
 
@@ -40,6 +42,8 @@ initModel =
     , active = False
     , routeParam = ""
     , isFetching = True
+    , listView = True
+    , tableView = False
     }
 
 
@@ -57,6 +61,8 @@ type Msg
     | ArtworkPage String
     | UsersGallery String
     | ClearGallery
+    | ListView
+    | TableView
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -80,6 +86,22 @@ update msg model =
 
         ClearGallery ->
             ( initModel, Cmd.none )
+
+        ListView ->
+            ( { model
+                | listView = True
+                , tableView = False
+              }
+            , Cmd.none
+            )
+
+        TableView ->
+            ( { model
+                | listView = False
+                , tableView = True
+              }
+            , Cmd.none
+            )
 
 
 decodeJson : String -> Model -> ( Model, Cmd Msg )
@@ -122,20 +144,29 @@ view model =
     else
         div [ class "main main--gallery" ]
             [ errorPanel model.error
+            , div []
+                [ ul []
+                    [ li [ onClick ListView ] [ text "List View" ]
+                    , li [ onClick TableView ] [ text "Table View" ]
+                    ]
+                ]
             , galleryHeader
-            , gallery model
+            , if model.listView then
+                galleryListView model
+              else
+                galleryTableView model
             ]
 
 
-gallery : Model -> Html Msg
-gallery { gallery } =
+galleryListView : Model -> Html Msg
+galleryListView { gallery } =
     gallery
-        |> List.map painting
+        |> List.map paintingListView
         |> div [ class "container" ]
 
 
-painting : GalleryItem -> Html Msg
-painting { artist, title, medium, year, dimensions, price, artworkImageFile, artworkId } =
+paintingListView : GalleryItem -> Html Msg
+paintingListView { artist, title, medium, year, dimensions, price, artworkImageFile, artworkId } =
     div [ class "row" ]
         [ div [ class "artwork-container vertical-align" ]
             [ div [ class "col-sm-6 artwork-container__col-sm-6" ]
@@ -151,6 +182,37 @@ painting { artist, title, medium, year, dimensions, price, artworkImageFile, art
                     , button [ class "btn btn--green", onClick (ArtworkPage artworkId) ] [ text "view artwork" ]
                     ]
                 ]
+            ]
+        ]
+
+
+galleryTableView : Model -> Html Msg
+galleryTableView { gallery } =
+    gallery
+        |> List.map paintingTableView
+        |> tbody []
+        |> (\g -> galleryTableHeader :: [ g ])
+        |> table [ class "table table-striped" ]
+
+
+paintingTableView : GalleryItem -> Html Msg
+paintingTableView { artist, title, year, artworkImageFile, artworkId } =
+    tr []
+        [ td [] [ img [ src artworkImageFile, class "thumbnail" ] [] ]
+        , td [] [ text artist ]
+        , td [] [ a [ onClick (ArtworkPage artworkId) ] [ text title ] ]
+        , td [] [ text year ]
+        ]
+
+
+galleryTableHeader : Html Msg
+galleryTableHeader =
+    thead []
+        [ tr []
+            [ th [] [ text "Artwork" ]
+            , th [] [ text "Artist" ]
+            , th [] [ text "Title" ]
+            , th [] [ text "Year" ]
             ]
         ]
 
